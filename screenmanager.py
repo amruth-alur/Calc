be44ba15605d5c3ed2e45d70351226679d554ac3
Amruth <111494988+amruth-alur@users.noreply.github.com>
2023-12-21 19:14:02 +0530

    Update screenmanager.py

--- screenmanager.py
+++ screenmanager.py
@@ -1,26 +1,31 @@
+# screenmanager.py
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
-import sympy as sp
-import time
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.dialog import MDDialog
 from kivy.properties import StringProperty
-
-class FirstPage(Screen):
-    expression = StringProperty()
-
-    def add_to_expression(self, value):
-        self.expression += str(value)
-
-    def clear_expression(self):
-        self.expression = ""
-
-    def calculate(self):
-        try:
-            self.expression = str(eval(self.expression))
-        except Exception:
-            self.expression = "Error"
+from kivymd.toast import toast
 
 class MainScreenManager(ScreenManager):
     pass
+
+class FirstPage(Screen):
+    expression = StringProperty("")  # User-entered expression
+    result = StringProperty("")  # Result of the calculation
+
+    def append(self, text):
+        self.expression += text  # Append character to expression
+
+    def clear(self):
+        self.expression = ""  # Clear expression
+        self.result = ""  # Clear result
+
+    def calculate(self):
+        try:
+            self.result = str(eval(self.expression))  # Evaluate expression and set result
+        except SyntaxError:
+            toast("Invalid input")  # Show toast for syntax error
+            self.clear()
+        except ZeroDivisionError:
+            toast("Cannot divide by zero")  # Show toast for division by zero
+            self.clear()
+        except Exception:
+            toast("Something went wrong")  # Show toast for other errors
+            self.clear()

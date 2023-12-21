afa7aa9a2f7517a90c262239ffc1be5e82f96c8e
asimgeek <asim2018off@gmail.com>
2023-12-21 20:18:50 +0530

    Refactor expression calculation in FirstPage class

--- screenmanager.py
+++ screenmanager.py
@@ -1,11 +1,7 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
 import sympy as sp
-import time
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.dialog import MDDialog
 from kivy.properties import StringProperty
+import re
 
 class FirstPage(Screen):
     expression = StringProperty()
@@ -18,8 +14,35 @@ class FirstPage(Screen):
 
     def calculate(self):
         try:
-            self.expression = str(eval(self.expression))
-        except Exception:
+            expr = self.expression
+
+            # Replace text with sympy trigonometric functions
+            expr = expr.replace('sin', 'sp.sin')
+            expr = expr.replace('cos', 'sp.cos')
+            expr = expr.replace('tan', 'sp.tan')
+            expr = expr.replace('asin', 'sp.asin')
+            expr = expr.replace('acos', 'sp.acos')
+            expr = expr.replace('atan', 'sp.atan')
+
+            # Handle exponent (^)
+            expr = expr.replace('^', '**')
+
+            expr = expr.replace('pi', 'sp.pi')
+
+            expr = expr.replace('sqrt', 'sp.sqrt')
+            expr = expr.replace('root', 'sp.root')
+            expr = expr.replace('log', 'sp.log')
+            
+            x = sp.symbols('x')
+            result = eval(expr, {"sp": sp, "x": x})
+            
+            # If the result is a sympy object, convert it to a float
+            if isinstance(result, sp.Basic):
+                result = float(result.evalf())
+
+            self.expression = str(result)
+        except Exception as e:
+            print(e)
             self.expression = "Error"
 
 class MainScreenManager(ScreenManager):

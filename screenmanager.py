04de5e605e00ff0062b7543c6fa48a30fb5d04b7
Amruth <111494988+amruth-alur@users.noreply.github.com>
2023-12-29 10:11:54 +0530

    Update screenmanager.py

--- screenmanager.py
+++ screenmanager.py
@@ -1,26 +1,37 @@
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import ObjectProperty
 import sympy as sp
-import time
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.dialog import MDDialog
 from kivy.properties import StringProperty
 
 class FirstPage(Screen):
-    expression = StringProperty()
-
-    def add_to_expression(self, value):
-        self.expression += str(value)
-
-    def clear_expression(self):
-        self.expression = ""
+	@@ -18,8 +13,31 @@ def clear_expression(self):
 
     def calculate(self):
         try:
-            self.expression = str(eval(self.expression))
-        except Exception:
+            expr = self.expression
+            # Then replace trigonometric functions
+            expr = expr.replace('sin', 'sp.sin')
+            expr = expr.replace('cos', 'sp.cos')
+            expr = expr.replace('tan', 'sp.tan')
+
+            # Handle exponent (^)
+            expr = expr.replace('^', '**')
+
+            expr = expr.replace('pi', 'sp.pi')
+
+            expr = expr.replace('sqrt', 'sp.sqrt')
+            expr = expr.replace('root', 'sp.root')
+            expr = expr.replace('log', 'sp.log')
+
+            x = sp.symbols('x')
+            result = eval(expr, {"sp": sp, "x": x})
+
+            # If the result is a sympy object, convert it to a float
+            if isinstance(result, sp.Basic):
+                result = float(result.evalf())
+
+            self.expression = str(result)
+        except Exception as e:
+            print(e)
             self.expression = "Error"
 
 class MainScreenManager(ScreenManager):
-    pass
